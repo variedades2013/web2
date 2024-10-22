0ab3200d29df3be0dede5e82f25b02460aa5dc40
variedades2013 <figue.1552@gmail.com>
2024-10-22 08:13:52 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>suscripción</title>
+    <link rel="stylesheet" href="sups.css">
+    <link rel="stylesheet" href="sups.css">
+    <link rel="stylesheet" href="npm i bootstrap-icons">
+
+</head>
+<body class="cuerpo">
+
+
+<form method="post">
+
+<h2><b>Bienvenido</b></h2>
+<p><b>Aqui puedes realizar tu suscripción:</b></p>
+<div class="input-wrapper">
+<input type="text" name="nombre" placeholder="Nombre">
+<img class="input-icon" src="person-circle.svg" alt="">
+</div>
+
+<div class="input-wrapper">
+<input type="text" name="apellido" placeholder="Apellido">
+<img class="input-icon" src="person-circle.svg" alt="">
+</div>
+
+<div class="input-wrapper">
+<input type="tel" name="telefono" placeholder="Teléfono">
+<img class="input-icon" src="telephone.svg" alt="">
+</div>
+
+<div class="input-wrapper">
+<input type="email" name="correo" placeholder="Correo Electrónico">
+<img class="input-icon" src="envelope-at.svg" alt="">
+</div>
+
+<div class="input-wrapper">
+<input type="tel" name="edad" placeholder="Edad">
+<img class="input-icon" src="calendar-check.svg" alt="">
+</div>
+
+<input class="btn" type="submit" name="register" value="Enviar">
+
+</form>
+
+
+    
+</body>
+</html>
